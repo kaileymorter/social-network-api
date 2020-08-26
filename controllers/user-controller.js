b354bea8424a5a65e82289854aa912ab3ff53639
Spartacus <k_morter@outlook.com>
2020-08-25 18:48:36 -0600

    added minor changes to user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,34 +1,49 @@
 const { User } = require('../models');
 
+//set up user controller
 const userController = {
     //get all users
     getAllUser(req, res) {
         User.find({})
             .populate({
-                path: 'thoughts',
+                //popuate users thoughts
+                path: 'thought',
+                select: '-__v'
+            })
+            .populate({
+                //popuate users friends
+                path: 'friends',
                 select: '-__v'
             })
             .select('-__v')
-            .sort({ _id: 1 })
+            .sort({ _id: -1 })
             .then(dbUserData => res.json(dbUserData))
             .catch(err => {
                 console.log(err);
                 res.sendStatus(400);
             })
     },
-    //get one user by id
+    //get single user by id
     getUserById({ params }, res) {
-        User.findOne({ _id: params.is })
+        User.findOne({ _id: params.id })
             .populate({
-                path: 'thoughts',
+                path: 'thought',
                 select: '-__v'
             })
+            .populate({
+                //popuate users friends
+                path: 'friends',
+                select: '-__v'
+            })  
             .select('-__v')
-            .then(dbUserData => res.json(dbUserData))
-            .catch(err => {
-                console.log(err);
-                res.sendStatus(400);
+            .then(dbUserData => {
+                if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+                }
+                res.json(dbUserData);
             })
+            .catch(err => res.status(400).json(err));
     },
     //create user
     createUser({ body }, res) {
