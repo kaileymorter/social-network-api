c494dc02f74574df38cb5dbda09f8f439eb3bf3f
Spartacus <k_morter@outlook.com>
2020-08-25 20:22:01 -0600

    updated server.js file

--- server.js
+++ server.js
@@ -10,12 +10,13 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
     useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 mongoose.set('debug', true);
+//mongoose.set('useCreateIndex', 'debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
